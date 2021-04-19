71268d83d67ea0b8fc07b167807a49fb2cd0f0fd
Ana Knickerbocker <ana.knickerbocker@gmail.com>
2021-04-19 23:05:05 +0000

    Added working code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+## API Project: File Metadata Microservice
+
+### User Stories:
+
+1.  I can submit a form object that includes a file upload.
+2.  The from file input field has the "name" attribute set to "upfile". We rely on this in testing.
+3.  When I submit something, I will receive the file name, and size in bytes within the JSON response.
+
+### Usage:
+
+See a working example at: [https://ink-profit.glitch.me/](https://ink-profit.glitch.me/)
+
+[freeCodeCamp](http://www.freecodecamp.com)
--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+'use strict';
 
-var app = express();
+import express from 'express'
+import cors from 'cors'
+import multer from 'multer'
+import dotenv from 'dotenv'
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: 'uploads/' });
+const app = express()
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+dotenv.config()
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
+app.get('/', (_req, res) => {
+    res.sendFile(process.cwd() + '/views/index.html')
+})
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, _next) => {
+    res.json({
+        'original file name': req.file.originalname,
+        'file name': req.file.filename,
+        size: req.file.size,
+    })
+})
 
+app.get('/hello', (_req, res) => {
+    res.json({ greetings: 'Hello, API' })
+})
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('Your app is listening on port ' + port)
+})
